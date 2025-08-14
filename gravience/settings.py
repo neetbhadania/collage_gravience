d003377ebc99f0ef65577a637c897345a2901d39
neetbhadania <neetbhadania155@gmail.com>
2025-08-14 19:16:37 +0530

    commit

--- gravience/settings.py
+++ gravience/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR,'Templates')
-# STATICFILES_DIR = os.path.join(BASE_DIR,'static')
+
 
 
 # Quick-start development settings - unsuitable for production
